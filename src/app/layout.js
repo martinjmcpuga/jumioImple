4cdef088c86e4d6cbf98dc13aa4fc9ebbe2a4265
martin <martin@martin-ThinkPad-T460s>
2025-08-27 19:43:05 -0600

    espacio

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,31 +6,29 @@ import inter from "./components/Fonts/Fonts";
 import { AppProvider } from './context/AppContext';
 import { Suspense } from "react";
 
-  export const metadata = {
+export const metadata = {
   title: 'DPR',
   icons: {
     icon: '/dpr_lab_logo.ico',
   }
 };
 
-
 export default function RootLayout({ children }) {
-
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <AppProvider>
-    <html lang="en">
+      <AppProvider>
+        <html lang="en">
 
-      <body
-        className={` antialiased ${inter.className} initBack_P2 animate__animated animate__fadeIn`}
-      >
-        <Header imgBack={true} />
-        {children}
-       
-      </body>
-    </html>
-    </AppProvider>
+          <body
+            className={` antialiased ${inter.className} initBack_P2 animate__animated animate__fadeIn`}
+          >
+            <Header imgBack={true} />
+            {children}
+
+          </body>
+        </html>
+      </AppProvider>
     </Suspense>
   );
 }
