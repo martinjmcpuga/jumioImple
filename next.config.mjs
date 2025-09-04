ca8b0b27fa21f3a55ec62cab9e4ee6c5a76f083c
martin <martin@martin-ThinkPad-T460s>
2025-09-03 18:23:26 -0600

    config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                canvas: false,
+            };
+
+            config.externals.push({
+                canvas: 'canvas'
+            });
+        }
+
+
+        config.module.rules.push({
+            test: /\.node$/,
+            use: 'ignore-loader',
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
