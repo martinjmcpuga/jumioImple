a0c7c9ca8394a936bbd3ee572975c080a994d72b
martin <martin@martin-ThinkPad-T460s>
2025-09-06 20:21:56 -0600

    add npage

---
+++ src/app/actividadeslaborales/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import ActividadesLaborales from '../components/Pages/ActividadesLaborales/ActividadesLaborales'
+
+const Page = () => {
+  return (
+    <>
+      <ActividadesLaborales />
+    </>
+  )
+}
+
+export default Page
