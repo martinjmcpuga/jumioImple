0d23f977fb623b4562bc6bb12ee0627cc2c9805b
martin <martin@martin-ThinkPad-T460s>
2025-09-10 09:36:12 -0600

    comentario

--- src/app/components/jumio/JumioComponent.jsx
+++ src/app/components/jumio/JumioComponent.jsx
@@ -18,31 +18,9 @@ const JumioComponent = ({ token }) => {
 
   useEffect(() => {
 
-    //  const template = '<template  id="jumio-start-title">\n  <pre>You can see now projected content via <i><strong>jumio-start-title</strong></i> template</pre>\n</template>'
-
     const handleStatus = async (event) => {
 
-      // console.log(IdJumio)
-      // console.log('Workflow status:', event.detail);
-      //////////  const response = await ApiJumioRetrieval(IdJumio)
-      //  if(tokenJumio){
-      //    console.log(response)
-      //    console.log(response.status)
-
-      //    if(response.status === 200){
-      //        router.push('/bandeja');
-      //    }else{
-      //        router.push('/?i='+cpv);
-      //    }
-      //    return;
-
-      //   }else{
-
       router.push('/requerimientoselfie');
-
-      //   }
-
-      //console.log('Jumio status:', response);
 
     }
 
