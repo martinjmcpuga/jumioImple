0fede4669c00d3803f3f6087e625af19194bdb3a
martin <martin@martin-ThinkPad-T460s>
2025-10-19 15:15:39 -0600

    add class

--- src/app/components/Pages/UploadComprobanteN5/UploadFile.jsx
+++ src/app/components/Pages/UploadComprobanteN5/UploadFile.jsx
@@ -284,7 +284,7 @@ function UploadFile() {
           ) : (
 
             <div className='animate__animated animate__fadeIn'>
-              <div className='containerInfo_P2'>
+              <div className='containerInfo_P2 onContentExpands'>
                 <div className="containerIdent_P2">
                   {filesImage.length > 0 ? (
                     <div>
